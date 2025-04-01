e42d9a69f8c10b97ade0c34da16277350d477aca
LLorintz <lorintz.levente@mechwart.com>
2025-04-01 15:54:22 +0200

    Update Home.tsx

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 const Home = () => {
+    {/*A komponens megírásához szükséges css osztályokat a home.css-ben találod.*/}
+    
+  {/*A kép több másik képből tevődik össze ezeket egy tömbben találod a data mappában.*/}
+
+  {/*A kép hátterében egy alakzat van.*/}
+
+  {/*A gomb mutasson az about részre */}
   return (
     <div>Home</div>
   )
