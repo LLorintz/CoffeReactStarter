65f93ad51e7d1d7592e99e6c4ddacd3bdfa85ec1
LLorintz <lorintz.levente@mechwart.com>
2025-04-01 16:37:45 +0200

    Update Popular.tsx

--- src/components/Popular/Popular.tsx
+++ src/components/Popular/Popular.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const Popular = () => {
+  {/*Fetcheld be a popular.json-ból az adatokat és tárold le egy állapotváltozóban*/}
+
+  {/*Hozz létre egy popularcard komponenst és a lementett adatokat ezen keresztül jelenítsd meg*/}
   return (
     <div>Popular</div>
   )
