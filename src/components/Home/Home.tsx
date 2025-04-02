65f0f0b2cd61a0942764d0207ebfb2907cb67af1
LLorintz <lorintz.levente@mechwart.com>
2025-04-02 14:52:07 +0200

    Update Home.tsx

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,15 +1,32 @@
 import React from 'react'
 
 const Home = () => {
-    {/*A komponens megírásához szükséges css osztályokat a home.css-ben találod.*/}
+  {/*A komponens megírásához szükséges css osztályokat a home.css-ben találod.*/}
     
   {/*A kép több másik képből tevődik össze ezeket egy tömbben találod a data mappában.*/}
 
   {/*A kép hátterében egy alakzat van.*/}
 
   {/*A gomb mutasson az about részre */}
+
+  {/*Egészítsd ki a hiányzó részekkel */}}
   return (
-    <div>Home</div>
+    <div className="" id="home">        
+            <h1 className=""></h1>
+            <div className="">
+                {/*ide illeszd be az alakzatot*/}
+                {/*ide illeszd be a képeket*/}
+            </div>
+            <img src="/img/home-sticker.svg" alt="Home sticker" className="home__sticker" />
+        <div className="home__data">
+            <p className="home__description">
+                Find delicious hot and cold coffees with the
+                best varieties, calm the pleasure and enjoy
+                a good coffee, order now.
+            </p>
+            <a href="#about" className="button">Learn More</a>
+        </div>        
+    </div>
   )
 }
 
